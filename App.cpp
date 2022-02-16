cd65524bfd6a936b2612302c029d371ff21acd13
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-02-16 09:05:56 +0700

    StrukturData - App.cpp | Menghapus function dummy

--- App.cpp
+++ App.cpp
@@ -31,22 +31,6 @@ void tampilKlasemen(Klasemens Klasemen[]){
 	cout << "=====================================================" << endl;
 }
 
-void dummyKlasemen(Klasemens Klasemen[]) {
-  for ( int i = 0; i < 16; i++ ) {
-    cout << "Data ke-" << i + 1 << endl;
-    cout << "==========" << endl;
-    cout << "Nama Klub: " << Klasemen[i].nama_klub << endl;
-    cout << "Main: " << Klasemen[i].main << endl;
-    cout << "Menang: " << Klasemen[i].menang << endl;
-    cout << "Kalah: " << Klasemen[i].kalah << endl;
-    cout << "Seri: " << Klasemen[i].seri << endl;
-    cout << "Memasukkan: " << Klasemen[i].memasukkan << endl;
-    cout << "Kemasukkan: " << Klasemen[i].kemasukkan << endl;
-    cout << "Nilai: " << Klasemen[i].nilai << endl;
-    cout << "\n";
-  }
-}
-
 void initKlasemen(Klasemens Klasemen[]) {
 
   for ( int i = 0; i < 16; i++ ) {
@@ -77,7 +61,6 @@ int main() {
   Klasemens Klasemen[16];
 
   initKlasemen(Klasemen);
-  //dummyKlasemen(Klasemen);
   tampilKlasemen(Klasemen);
 
   return 0;
